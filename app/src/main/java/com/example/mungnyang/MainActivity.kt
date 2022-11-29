16da5ce7959f19313704b4114b72b74ad68edc28
Chae-eun Kim <chaeun2066@gmail.com>
2022-11-29 22:05:31 +0900

    [UPDATE] kce_feature MainActivity.kt override fun onCreateOptionsMenu / onOptionsItemSelected

--- app/src/main/java/com/example/mungnyang/MainActivity.kt
+++ app/src/main/java/com/example/mungnyang/MainActivity.kt
@@ -3,15 +3,24 @@ package com.example.mungnyang
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.example.mungnyang.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Toolbar 설정
+        setSupportActionBar(binding.toolbar)
     }
 
+    //Toolbar 메뉴 기능 설정
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu,menu)
 
@@ -22,5 +31,12 @@ class MainActivity : AppCompatActivity() {
             true
         }
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
