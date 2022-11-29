eb76ba44ba7c3465d96e627afc7c161fa816a685
Chae-eun Kim <chaeun2066@gmail.com>
2022-11-30 00:42:57 +0900

    [CREATE] kce_feature FriendslistActivity Needs Files

---
+++ app/src/main/java/com/example/mungnyang/FriendslistActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mungnyang
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FriendslistActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friendslist)
+    }
+}
