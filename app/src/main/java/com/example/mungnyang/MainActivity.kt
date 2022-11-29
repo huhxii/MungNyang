e9fb7f2f9cb528f9fb693417f9751defb7828915
Chae-eun Kim <chaeun2066@gmail.com>
2022-11-29 17:44:57 +0900

    [UPDATE] kce_feature MainActivity.kt add fun onCreateOptionsMenu

--- app/src/main/java/com/example/mungnyang/MainActivity.kt
+++ app/src/main/java/com/example/mungnyang/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.mungnyang
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toast
 import com.example.mungnyang.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+
+        val menuItem = menu?.findItem(R.id.menu_chat)
+
+        menuItem?.setOnMenuItemClickListener {
+            Toast.makeText(applicationContext,"Direction Message로 이동합니다.", Toast.LENGTH_SHORT).show()
+            true
+        }
+        return super.onCreateOptionsMenu(menu)
     }
 }
