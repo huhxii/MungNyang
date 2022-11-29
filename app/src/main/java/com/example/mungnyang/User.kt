df9896a5ac52b4290744306b73300899edfde6a9
Chae-eun Kim <chaeun2066@gmail.com>
2022-11-29 23:32:22 +0900

    [UPDATE] kce_feature activity_login.xml change edit Inputtype

--- app/src/main/java/com/example/mungnyang/User.kt
+++ app/src/main/java/com/example/mungnyang/User.kt
@@ -1,3 +1,10 @@
 package com.example.mungnyang
 
-data class User()
+data class User(
+    var name: String,
+    var email: String,
+    var phone: String,
+    var uId: String
+){
+    constructor(): this("","","","")
+}
