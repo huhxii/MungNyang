9f20cc7b7770289507becff7af6f0254acac6a6d
seopit95 <dskim2104@gmail.com>
2022-11-29 15:46:59 +0900

    [UPDATE] kds_feature RegisterActivity.kt add 'Glide' and 'tv_RA_login.setOnClickListener Event'

--- app/src/main/java/com/example/mungnyang/RegisterActivity.kt
+++ app/src/main/java/com/example/mungnyang/RegisterActivity.kt
@@ -1,11 +1,23 @@
 package com.example.mungnyang
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.example.mungnyang.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        val binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Glide.with(this).load(R.raw.register).override(700,700).into(binding.ivRAMung)
+
+        binding.tvRALogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
