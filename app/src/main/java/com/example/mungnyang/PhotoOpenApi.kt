6b95b91b820fc510fc4c0ace0c282b32d79b15b1
Chae-eun Kim <chaeun2066@gmail.com>
2022-12-01 17:46:55 +0900

    [CREATE] kce_feature Api Example Files

---
+++ app/src/main/java/com/example/mungnyang/PhotoOpenApi.kt
@@ -0,0 +1,21 @@
+package com.example.mungnyang
+
+import com.example.mungnyang.animalPhoto.AnimalPhoto
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+//http://openapi.seoul.go.kr:8088/764a637071636f6438376d6c707a54/json/TbAdpWaitAnimalPhotoView/1/5/
+
+class PhotoOpenApi {
+    companion object{
+        const val DOMAIN = "http://openapi.seoul.go.kr:8088"
+        const val API_KEY = "764a637071636f6438376d6c707a54"
+        const val LIMIT = 300
+    }
+}
+
+interface PhotoOpenService{
+    @GET("{api_key}/json/TbAdpWaitAnimalPhotoView/1/{end}")
+    fun getAnimal(@Path("api_key") key : String, @Path("end") limit : Int): Call<AnimalPhoto>
+}
